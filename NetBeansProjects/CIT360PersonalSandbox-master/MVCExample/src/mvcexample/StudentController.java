284521ee47eb233c5e71108c6c555519ee65abbf
Sal Dababneh <dab13001@byui.edu>
2016-03-17 11:58:40 -0500

    Code updates

---
+++ NetBeansProjects/CIT360PersonalSandbox-master/MVCExample/src/mvcexample/StudentController.java
@@ -0,0 +1,40 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvcexample;
+
+/**
+ *
+ * @author sdababneh
+ */
+public class StudentController {
+   private Student model;
+   private StudentView view;
+
+   public StudentController(Student model, StudentView view){
+      this.model = model;
+      this.view = view;
+   }
+
+   public void setStudentName(String name){
+      model.setName(name);		
+   }
+
+   public String getStudentName(){
+      return model.getName();		
+   }
+
+   public void setStudentRollNo(String rollNo){
+      model.setRollNo(rollNo);		
+   }
+
+   public String getStudentRollNo(){
+      return model.getRollNo();		
+   }
+
+   public void updateView(){				
+      view.printStudentDetails(model.getName(), model.getRollNo());
+   }	
+}
