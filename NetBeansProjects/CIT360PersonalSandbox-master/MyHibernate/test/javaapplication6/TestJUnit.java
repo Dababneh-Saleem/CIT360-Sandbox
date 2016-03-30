5a888f24b71c021cca89ad1deb0df459d7f988f4
Sal Dababneh <dab13001@byui.edu>
2016-03-30 16:46:02 -0500

    Code updates

--- NetBeansProjects/CIT360PersonalSandbox-master/MyHibernate/test/javaapplication6/TestJUnit.java
+++ NetBeansProjects/CIT360PersonalSandbox-master/MyHibernate/test/javaapplication6/TestJUnit.java
@@ -1,10 +1,12 @@
 package javaapplication6;
 
 import java.util.List;
-import javaapplication6.Employee;
-import javaapplication6.ManageEmployee;
 import static junit.framework.Assert.assertNotNull;
+import static junit.framework.Assert.assertSame;
+import static junit.framework.Assert.assertNotSame;
+import static junit.framework.Assert.assertNull;
 import org.hibernate.HibernateException;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -54,10 +56,50 @@ public class TestJUnit  {
          Employee employee = new Employee("Zara", "Ali", 1000);
          employeeID = (Integer) session.save(employee); 
          tx.commit();
-           List employees = session.createQuery("FROM Employee").list();
+           List employees = null;    
+           assertNull(employees);
+            employees = session.createQuery("FROM Employee").list(); 
            assertNotNull(employees);
            assertEquals(1,employees.size());
-         
+           if(employees.size() == 1)
+           {
+               System.out.println("Employee "+employee);
+                System.out.println("List Employee "+employees.get(0));
+                assertSame(employee,employees.get(0));    
+                
+                Employee newemployee = new Employee("Zara", "Ali", 1000);
+                 System.out.println("New Employee "+newemployee);
+                assertNotSame(newemployee,employees.get(0));
+                
+           //Update Employee
+                 tx = session.beginTransaction();
+           String queryStr = "from Employee where id =:employeeId ";
+           Query query = session.createQuery(queryStr);
+           Employee dbEmployee = (Employee)employees.get(0);
+              System.out.println("Employee ID is "+dbEmployee.getId());
+           query.setInteger("employeeId",dbEmployee.getId() );
+           Employee updateEmployee = (Employee)query.uniqueResult();
+           updateEmployee.setSalary(5000);
+           session.update(updateEmployee);
+             tx.commit();
+           assertSame(employee,updateEmployee);
+           System.out.println("Employee is updated. First Name is "+newemployee.getFirstName());
+           
+           //Delete Employee 
+            tx = session.beginTransaction();
+           String queryString = "from Employee where id =:employeeId ";
+           Query deleteQuery = session.createQuery(queryString);         
+           deleteQuery.setInteger("employeeId",dbEmployee.getId() );
+           Employee deleteEmployee = (Employee)deleteQuery.uniqueResult();
+            assertSame(employee,deleteEmployee);
+           session.delete(deleteEmployee);
+           tx.commit();
+           System.out.println("Employee "+deleteEmployee.getFirstName()+ " is deleted.");
+           
+           
+           }
+           
+        
       }catch (HibernateException e) {
          if (tx!=null) tx.rollback();
          e.printStackTrace(); 
