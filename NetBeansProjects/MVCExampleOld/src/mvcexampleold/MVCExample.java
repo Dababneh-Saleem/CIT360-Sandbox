b8b36769629a238adda53d51f54cd66e23f68a85
Sal Dababneh <dab13001@byui.edu>
2016-04-04 22:25:01 -0500

    Code updates

---
+++ NetBeansProjects/MVCExampleOld/src/mvcexampleold/MVCExample.java
@@ -0,0 +1,44 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package mvcexampleold;
+
+import mvcexampleold.Student;
+
+/**
+ *
+ * @author sdababneh
+ */
+public class MVCExample {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+         //fetch student record based on his roll no from the database
+      Student model  = retriveStudentFromDatabase();
+
+      //Create a view : to write student details on console
+      StudentView view = new StudentView();
+
+      StudentController controller = new StudentController(model, view);
+
+      controller.updateView();
+
+      //update model data
+      controller.setStudentName("John");
+
+      controller.updateView();
+       
+              
+    }
+    private static Student retriveStudentFromDatabase(){
+      Student student = new Student();
+      student.setName("Robert");
+      student.setRollNo("10");
+      return student;
+   }
+}
